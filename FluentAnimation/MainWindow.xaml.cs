6c177ab6f6e4a2e693ae231834d58a8a982ae323
Jame Floc'h Le Carour <81566176+JameFLC@users.noreply.github.com>
2025-03-10 23:11:13 +0100

    Add Connected Animations

--- FluentAnimation/MainWindow.xaml.cs
+++ FluentAnimation/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Composition;
 using System.Numerics;
+using WinRT;
+using Microsoft.UI.Xaml.Media.Animation;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -67,7 +70,46 @@ namespace FluentAnimation
                 new("Della Case", "128 Xaml Street", "Marketing"),
             };
 
-            BasicGridView.ItemsSource = GridElements;
+            InfoGridView.ItemsSource = GridElements;
+        }
+
+        private InfoItemData? _selectedData;
+
+        private void InfoGridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GridViewItem? selectedItem = InfoGridView.ContainerFromItem(InfoGridView.SelectedItem) as GridViewItem;
+
+            if (selectedItem is null) return;
+
+            _selectedData = selectedItem.Content as InfoItemData;
+
+            if (_selectedData is null) return;
+
+            SelectedNameTextBlock.Text = _selectedData.Name;
+            SelectedAddressTextBlock.Text = _selectedData.Address;
+            SelectedJobTextBlock.Text = _selectedData.Job;
+            
+            OverlayPopup.Visibility = Visibility.Visible;
+
+            InfoGridView.SelectedItem = null;
+
+            
+            ConnectedAnimation connectedAnimation = InfoGridView.PrepareConnectedAnimation("forwardAnimation", _selectedData, "MainBorder");
+
+            connectedAnimation.Configuration = new DirectConnectedAnimationConfiguration();
+            connectedAnimation.TryStart(DestinationElement);
+        }
+
+        private async void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            ConnectedAnimation connectedAnimation = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("backwardsAnimation", DestinationElement);
+
+            connectedAnimation.Configuration = new DirectConnectedAnimationConfiguration();
+            
+            await InfoGridView.TryStartConnectedAnimationAsync(connectedAnimation, _selectedData, "MainBorder");
+            
+            OverlayPopup.Visibility = Visibility.Collapsed;
         }
     }
 }
